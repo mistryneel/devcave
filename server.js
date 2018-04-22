5de5bd55b125ad08e00535d6b9e0eda6dffeaa06
Neel Mistry <npkmistry@gmail.com>
2018-04-21 20:31:30 -0700

    eslint setup

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require("express")
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello World"))
 
-const port = 5000;
+const port = 5000
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`))
