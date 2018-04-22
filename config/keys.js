44701934309c8c871ab054feb5361a1806294444
Neel Mistry <npkmistry@gmail.com>
2018-04-21 21:13:23 -0700

    mongoDB connection

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const keys = {
+  mongoURI: 'mongodb://neel:neel@ds149279.mlab.com:49279/devcave'
+}
+
+export default keys
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import keys from './config/keys.js'
 
 const app = express()
 
+// Connect Mongo DB
+const db = keys.mongoURI
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(`MongoDB failed to connect : ${err}`))
+
 app.get("/", (req, res) => res.send("Hello World"))
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
